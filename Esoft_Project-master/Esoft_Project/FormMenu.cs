97e4b7d0197675fc5368ebbe46622133e1a367e6
Алексей <66165755+TbI-FUFEL@users.noreply.github.com>
2020-06-03 18:17:37 +0300

    Add files via upload

---
+++ Esoft_Project-master/Esoft_Project/FormMenu.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Esoft_Project
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpenClients_Click(object sender, EventArgs e)
+        {
+            //Задаем новую форму из класса Клиент и открываем ее
+            Form formClient = new formClient();
+            formClient.Show();
+        }
+
+        private void buttonOpenAgents_Click(object sender, EventArgs e)
+        {
+            Form formRieltor = new FormRieltor();
+            formRieltor.Show();
+        }
+
+        private void buttonOpenRealEstates_Click(object sender, EventArgs e)
+        {
+            Form formRealEstate = new FormRealEstate();
+            formRealEstate.Show();
+        }
+
+        private void buttonOpenDemands_Click(object sender, EventArgs e)
+        {
+            Form formSlypply = new FormSupply();
+            formSlypply.Show();
+        }
+
+        private void buttonOpenSupplies_Click(object sender, EventArgs e)
+        {
+            Form formDemand = new FormDemand();
+            formDemand.Show();
+        }
+    }
+}
---
+++ Esoft_Project-master/Esoft_Project/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Esoft_Project
+{
+    static class Program
+    {
+        // создание статистического экземпляра класса модели ADD.EDM
+        public static TutorialEntities3 wtfDb = new TutorialEntities3();
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Menu());
+        }
+    }
+}
